707d67f962b93e30d748c123a337ee483b36952e
MET-DEV <mustafatpe23@gmail.com>
2022-01-29 12:25:37 +0300

    socket.io added

--- app/src/app.js
+++ app/src/app.js
@@ -2,6 +2,8 @@ const express=require("express")
 const config=require("./config");
 const loaders=require("./loaders")
 const helmet = require("helmet");
+const http = require('http');
+const socketio=require('socket.io')
 const {UserRoutes,ChatMessagesRoutes}=require("./routes")
 
 
@@ -12,9 +14,21 @@ loaders();
 
 
 const app=express()
+const server = http.createServer(app);
+const io=socketio(server)
+
+
 app.use(express.json())
 app.use(helmet())
-app.listen(process.env.APP_PORT,()=>{
+
+
+server.listen(process.env.APP_PORT,()=>{
     app.use("/users",UserRoutes)
     app.use("/messages",ChatMessagesRoutes)
+    io.on("connection",socket=>{
+        socket.on("MESSAGE_EVENT",message=>{
+            io.brodcast.emit("MESSAGE_ADDED",message)
+        })
+    })
+    
 })
